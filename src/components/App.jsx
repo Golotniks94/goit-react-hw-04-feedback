20019c721f8aa95c1a310b2981311e8294bf0811
Vladimirs <98471773+Golotniks94@users.noreply.github.com>
2023-02-20 16:54:07 +0200

    Push!!!

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
+import { useReducer } from 'react';
+// import { useState} from 'react';
+import { Section, Statistics, Notification, FeedbackOptions } from 'components';
+
+const initialValue = { good: 0, neutral: 0, bad: 0 };
+
+function reducer(state, action) {
+  console.log(action.type);
+  return { ...state, [action.type]: state[action.type] + 1 };
+}
+
+// useReducer
 export const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialValue);
+  const { good, neutral, bad } = state;
+
+  const countTotalFeedback = () => {
+    const values = Object.values(state);
+    return values.reduce((acc, value) => acc + value, 0);
+  };
+
+  const countPositiveFeedbackPercentage = (total, good) => {
+    const percentage = Math.round((good / total) * 100);
+    return percentage;
+  };
+
+  const total = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage(total, good);
+  const options = Object.keys(state);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={options} onClick={dispatch} />
+      </Section>
+
+      <Section title="Statistics">
+        {total > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </>
   );
 };
